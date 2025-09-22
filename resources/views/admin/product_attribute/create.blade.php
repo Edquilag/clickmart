1aa670b2144435b59f790f25d89f992e41ebd225
Glein Ardian <edquilaggleinardian@gmail.com>
2025-09-22 22:42:47 +0800

    Initial commit

---
+++ resources/views/admin/product_attribute/create.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layouts.layout')
+@section('admin_page_title')
+Create Product Atrribute - Admin Panel
+@endsection
+@section('admin_layout')
+<h3>Create Product Attributes Page</h3>
+
+@endsection
---
+++ resources/views/admin/product_attribute/manage.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layouts.layout')
+@section('admin_page_title')
+Manage Product Atrribute - Admin Panel
+@endsection
+@section('admin_layout')
+<h3>Manage Product Attributes</h3>
+
+@endsection
